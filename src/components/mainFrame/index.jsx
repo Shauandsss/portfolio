089d9432b9cfc4ee3c34dfd5283c135dfcbed464
shauan santos <shauands@gmail.com>
2022-02-13 18:10:24 -0500

    gh-pages

---
+++ src/components/mainFrame/index.jsx
@@ -0,0 +1,71 @@
+import './mainFrame.scss'
+import meImage from '../../images/Foto-web.png'
+import React, { useEffect, useState } from 'react'
+
+function Header( {func, mainColor, complementaryColor} ) {
+
+    useEffect(() => {
+        console.log('Calling func');
+        func();
+      }, []);
+
+    return (
+        <div className="mainFrame--main " id='c0'>
+            {<div className='bg' style={{backgroundColor: 'rgba(' + (mainColor.replace('rgb(','')).replace(')', '') + ', .7)'}}></div>}
+            <div className='meImage'>
+                    <img src={meImage}/> 
+            </div>
+            <div className='texts' 
+                 style={{color: complementaryColor,
+                         textShadow: '4px 4px 4px ' + mainColor}}>
+                <div className='title'>
+                    Hi,<br/>
+                    I'm Shauan,<br/>
+                    web developer.<br/>
+                </div>
+                <div class='content'>
+                    React js | Oracle db | Front end
+                </div>
+                
+            </div>
+            <div class='ball'>
+                <div class='layer moving'>
+                    <div class='layer gridplane xline'></div>
+                    <div class='layer gridplane xline2'></div>
+                    <div class='layer gridplane yline'></div>   
+                    <div class='layer gridplane zline'></div>  
+                </div> 
+                <div class='layer clip'>
+                    <div class='shade' 
+                    style={{background: 'radial-gradient(at 60% 60%, ' + mainColor + ', ' + complementaryColor + ', ' + mainColor ,
+                            transition: '2s all ease'
+                                            }}> 
+                    </div>
+                </div>
+            </div>
+            <div class='ball2 '>
+                <div class='layer2 moving'>
+                    <div class='layer2 gridplane xline'></div>
+                    <div class='layer2 gridplane xline2'></div>
+                    <div class='layer2 gridplane yline'></div>   
+                    <div class='layer2 gridplane zline'></div>  
+                </div> 
+                <div class='layer2 clip'>
+                    <div class='shade' 
+                    style={{background: 'radial-gradient(at 60% 60%, ' + mainColor + ', ' + complementaryColor + ', ' + mainColor ,
+                            transition: '2s all ease'
+                                            }}> 
+                    </div>
+                </div>
+            </div>
+            <div alt ='Change things'className='rainbow-button' 
+            style={{backgroundColor: mainColor,
+                    color: complementaryColor,
+                    textShadow: '4px 4px 4px ' + mainColor}} onClick={func}>
+            </div>
+    </div>
+    );
+  }
+  
+  export default Header;
+  
---
+++ src/components/scroll/index.jsx
@@ -0,0 +1,70 @@
+import './scroll.css'
+//import biohazard from '../../images/biohazard.png'
+//import backgroundC1 from '../../images/PNeQrc.png'
+import React, { useEffect, useState } from 'react'
+
+function Scroll() {
+
+    const[bottleFill, setBottleFill] = useState(10) 
+
+    useEffect (() => {
+        
+    }, [bottleFill])
+
+    function fill () {
+        setBottleFill(bottleFill < 100 ? bottleFill + 10 : bottleFill)
+    }
+
+    return (
+        <div className="container">
+
+
+            <div>
+                <div id='c1' className="container-item c1">
+                    
+                    <div className='bottle' onMouseOver={fill} >
+                        
+                        <div className='liquid' style={{height: bottleFill + "%"}} >
+                    
+                            <div class="bubbles" style={{opacity: bottleFill > 50? '1': '0'}}>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b2" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b5" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b6" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b3" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b7" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b4" />
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/88939/drop.png" class="bubble b8" />
+                            </div>
+
+                        </div>
+                        
+                       {/*<img src={biohazard} className="logo-biohazard" />*/}
+                    </div>
+                    {/*<img src={backgroundC1} className="background"/>*/}
+                </div>
+
+
+                <div id='c2' className="container-item c2"></div>
+                <div id='c3' className="container-item c3"></div>
+                <div id='c4' className="container-item c4"></div>
+                <div id='c5' className="container-item c5"></div>
+                <div id='c6' className="container-item c6"></div>
+                <div id='c7' className="container-item c7"></div>
+                <div id='c8' className="container-item c8"></div>
+                <div id='c9' className="container-item c9"></div>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Scroll;
+  
+  /*
+    A Art one;
+    A Invention one;
+    A Nordic one;
+    A Greek
+    A Vaporwave 
+   
+  */
