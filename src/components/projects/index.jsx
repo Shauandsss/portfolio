fc614338f4bd2b3aac111c56b535d21e4aab162a
shauan santos <shauands@gmail.com>
2022-02-28 04:03:40 -0500

    Project page w/ gh repos

---
+++ src/components/projects/index.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import './projects.css';
+import api from '../../api'
+import ProjectCard from '../projectCard';
+
+export default () => {
+
+    const [reposList, setReposList] = useState(null)
+
+    useEffect (() => {
+        const loadAll = async () => {
+            // Pegando a lista TOTAL
+            let list = await api.getListRepos();
+            list[0].items.sort(function(a,b){
+                return new Date(b.created_at) - new Date(a.created_at);
+            })
+            setReposList(list[0]);
+        }
+        loadAll();
+    }, []);
+
+    return(
+        <div className='projects'>
+            {reposList && console.log(reposList.items)}
+            {reposList && reposList.items.map((item,key)=>(
+                <ProjectCard key={item.id} data={item}></ProjectCard>
+            ))}
+        </div>
+    )
+}
